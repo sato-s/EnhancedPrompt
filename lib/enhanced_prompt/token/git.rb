13cbe98dd0cd9a1ac6b01432aecbbcee5a72699f
sato-s <s.sato.desu@gmail.com>
2016-05-18 19:51:27 +0900

    git scaffold

--- lib/enhanced_prompt/token/git.rb
+++ lib/enhanced_prompt/token/git.rb
@@ -1,23 +1,36 @@
-# TODO should be implemented someday
+require 'open3'
 class EnhancedPrompt::Prompt
   class Token::Git
 
-    def git(prefix:'',suffix:'')
-      _current_branch
+    def initialize(dir)
+      @dir = dir
+    end
+
+    def git(prefix:'(',suffix:')')
+      _call_git_branch
     end
 
     # TODO this could not show git branch
     # I should run %x|git branch| in Dir.pwd
     private
+
+    def _call_git_branch
+      stdout,stderr,status = *Open3.capture3("cd #{@dir};git branch")
+      if stdout =~ /^[*]\s(\w+)/ && status.exitstatus == 0 then
+        @_current_branch = $1
+      else
+        ''
+      end
+    end
+
     def _current_branch
       if @_current_branch.nil? then
         begin
-          result = %x|git branch|
-          if $?.exitstatus =! 0 then
-            return ''
+          result = %x|cd #{@dir};git branch|
+          if result =~ /^[*]\s(\w+)/ then
+            @_current_branch = $1
           else
-            current_branch_line = result[/^[*]+/]
-            @_current_branch = current_branch_line
+            ''
           end
         rescue =>e
           return ''
