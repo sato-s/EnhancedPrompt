6249739f657af4d414352c79a65bfbc054b9c668
sato-s <s.sato.desu@gmail.com>
2016-08-08 23:16:32 +0900

    change default2 -> default

--- lib/enhanced_prompt/style/style.rb
+++ lib/enhanced_prompt/style/style.rb
@@ -1,16 +1,9 @@
 class EnhancedPrompt::Prompt
   module Style
 
-    def default2
-      user.c(:white).bg(:dodger_blue) + '@'.c(:white).bg(:dodger_blue) +
-          ip.c(:white).bg(:dodger_blue) +"  ".bg(:dodger_blue)+
-          dir_abbreviated2.c(:white).bg(:dodger_blue) + "\n"+
-          "->".c(:dark_cyan)
-    end
-
     def default
-      time1.c(:green)+ " #{user} ".c(:aqua)+ git.c(:red)+ "\n"+
-      "#{dir_abbreviated2(20)} $".c(:white)
+      (user + '@' + ip + ' ' + dir + "\n" + '->')
+        .c(:white).bg.(:dodgerblue)
     end
 
   end
